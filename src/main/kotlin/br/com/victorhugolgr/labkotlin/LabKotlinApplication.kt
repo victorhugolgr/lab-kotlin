829c99d31baae896de5f7f9f536b8b9d6ff8e00c
Victor Hugo <victorhugolgr@gmail.com>
2022-02-08 22:33:38 -0300

    Primeiro commit

---
+++ src/main/kotlin/br/com/victorhugolgr/labkotlin/LabKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.victorhugolgr.labkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/victorhugolgr/labkotlin/LabKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.victorhugolgr.labkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
